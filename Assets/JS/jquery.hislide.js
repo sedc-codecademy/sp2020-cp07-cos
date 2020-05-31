0efc5388012d2077fd6629ba827250dbb3e6a061
Spase007 <56656096+Spase007@users.noreply.github.com>
2020-05-31 22:48:51 +0200

    Update jquery.hislide.js

--- Assets/JS/jquery.hislide.js
+++ Assets/JS/jquery.hislide.js
@@ -1,21 +1,12 @@
 (function($) {
-    // 本函数每次调用只负责一个轮播图的功能
-    // 也就是说只会产生一个轮播图，这个函数的作用域只能分配给一个轮播图
-    // 要求在调用本函数的时候务必把当前轮播图的根标签传递过来
-    // 这里的形参 ele 就是某个轮播的根标签
     var slide = function(ele,options) {
         var $ele = $(ele);
-        // 默认设置选项
         var setting = {
-        		// 控制轮播的动画时间
             speed: 1000,
-            // 控制 interval 的时间 (轮播速度)
             interval: 2000,
             
         };
-        // 对象合并
         $.extend(true, setting, options);
-        // 规定好每张图片处于的位置和状态
         var states = [
             { $zIndex: 1, width: 120, height: 150, top: 69, left: 134, $opacity: 0.2 },
             { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.4 },
@@ -29,7 +20,6 @@
         var $lis = $ele.find('li');
         var timer = null;
 
-        // 事件
         $ele.find('.hi-next').on('click', function() {
             next();
         });
@@ -46,9 +36,7 @@
 
         move();
         autoPlay();
-
-        // 让每个 li 对应上面 states 的每个状态
-        // 让 li 从正中间展开
+        
         function move() {
             $lis.each(function(index, element) {
                 var state = states[index];
@@ -56,9 +44,7 @@
             });
         }
 
-        // 切换到下一张
         function next() {
-            // 原理：把数组最后一个元素移到第一个
             states.unshift(states.pop());
             move();
         }
@@ -67,12 +53,11 @@
             timer = setInterval(next, setting.interval);
         }
     }
-    // 找到要轮播的轮播图的根标签，调用 slide()
     $.fn.hiSlide = function(options) {
         $(this).each(function(index, ele) {
             slide(ele,options);
         });
-        // 返回值，以便支持链式调用
+        
         return this;
     }
 })(jQuery);
